a1777dd8cb7ef639a4de751322b838c8264c466e
Patrick Violette <violette.patrick718@gmail.com>
2014-03-03 17:49:02 -0500

    typos woo

--- index.js
+++ index.js
@@ -5,24 +5,30 @@ var everyauth = require('everyauth');
 everyauth.debug = true;
 
 users = {}; // object of UserID -> User
-everyauth.facebook
-    .appId(process.env.FB_APP_ID)
-    .appSecret(fbCreds.appSecret)
-    .findOrCreateUser(function(session, accessToken, accessTokenExtra, fbUserMetadata) {
-        console.log("HI");
-        var promise = this.Promise();
-        if(users[fbUserMetadata.id]) {
-            promise.fulfill(users[fbUserMetadata.id]);
-        }else { // create user on server
-            users[fbUserMetadata.id] = {
-                id: fbUserMetadata.id,
-                name: fbUserMetadata.name,
-                email: fbUserMetadata.email,
-                facebook: fbUserMetadata
-            };
-            promise.fulfill(users[fbUserMetadata.id]);
-        }
-    }).redirectPath('/user');
+app.configure('production', function() {
+    everyauth.facebook
+        .appId(process.env.FB_APP_ID)
+        .appSecret(process.env.FB_APP_SECRET)
+        .findOrCreateUser(function(session, accessToken, accessTokenExtra, fbUserMetadata) {
+            console.log("HI");
+            var promise = this.Promise();
+            if(users[fbUserMetadata.id]) {
+                promise.fulfill(users[fbUserMetadata.id]);
+            }else { // create user on server
+                users[fbUserMetadata.id] = {
+                    id: fbUserMetadata.id,
+                    name: fbUserMetadata.name,
+                    email: fbUserMetadata.email,
+                    facebook: fbUserMetadata
+                };
+                promise.fulfill(users[fbUserMetadata.id]);
+            }
+        }).redirectPath('/user');
+});
+
+app.configure('development', function() {
+
+});
 
 app.use(express.bodyParser())
    .use(express.logger())
