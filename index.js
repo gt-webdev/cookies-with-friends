dd5624df43c892dddc6b3ab70958a758d7c7404b
Patrick Violette <violette.patrick718@gmail.com>
2014-03-03 21:51:58 -0500

    fb authentication works :) adding mongodb

--- index.js
+++ index.js
@@ -3,40 +3,53 @@ var app = express();
 var everyauth = require('everyauth');
 
 everyauth.debug = true;
+var mongo = require('mongodb');
+
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/cookie-friends';
 
 users = {}; // object of UserID -> User
-app.configure('production', function() {
-    everyauth.facebook
-        .appId(process.env.FB_APP_ID)
-        .appSecret(process.env.FB_APP_SECRET)
-        .findOrCreateUser(function(session, accessToken, accessTokenExtra, fbUserMetadata) {
-            console.log("HI");
-            var promise = this.Promise();
-            if(users[fbUserMetadata.id]) {
-                promise.fulfill(users[fbUserMetadata.id]);
-            }else { // create user on server
-                users[fbUserMetadata.id] = {
-                    id: fbUserMetadata.id,
-                    name: fbUserMetadata.name,
-                    email: fbUserMetadata.email,
-                    facebook: fbUserMetadata
-                };
-                promise.fulfill(users[fbUserMetadata.id]);
-            }
-        }).redirectPath('/user');
-});
+everyauth.facebook
+    .appId(process.env.FB_APP_ID)
+    .appSecret(process.env.FB_APP_SECRET)
+    .scope('email')
+    .fields('id,name,email,picture') 
+    .handleAuthCallbackError( function(req, res) {
+        console.log(req);
+    })
+    .findOrCreateUser(function(session, accessToken, accessTokenExtra, fbUserMetadata) {
+        console.log("HI");
+        var promise = this.Promise();
+        if(users[fbUserMetadata.id]) {
+            promise.fulfill(users[fbUserMetadata.id]);
+        }else { // create user on server
+            users[fbUserMetadata.id] = {
+                id: fbUserMetadata.id,
+                name: fbUserMetadata.name,
+                email: fbUserMetadata.email,
+                facebook: fbUserMetadata
+            };
+            promise.fulfill(users[fbUserMetadata.id]);
+        }
+        return promise;
+    })
+    .findUserById(function() {
+        console.log(arguments); 
+    })
+    .redirectPath('/user');
 
-app.configure('development', function() {
-
-});
+console.log(everyauth.facebook.configurable());
 
 app.use(express.bodyParser())
    .use(express.logger())
    .use(express.favicon())
    .use(express.cookieParser())
-   .use(express.session({secret: "SECRET"}))
-   .use(everyauth.middleware(app))
-   .use(app.router);
+   .use(express.session({secret: process.env.SECRET || "SECRET"}))
+   .use(everyauth.middleware(app)) // req.user, 
+   .use(app.router); // use app.get( path, fn)
+
+app.set('view engine', 'ejs');
 
 everyauth.everymodule.findUserById(function(userId, callback) {
    console.log("FIND USER");
@@ -55,8 +68,9 @@ app.get("/", function(req, res) {
 });
 
 app.get("/user", function(req, res) {
-    res.send("HELLO " + req.user);
+    res.send("HELLO " + JSON.stringify(req.user));
 });
+
 app.listen(process.env.PORT || 8080, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
